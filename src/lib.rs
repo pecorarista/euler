8c64c647561dd0e151c3ad507c86c7bc27a9ba8e
Miyazawa Akira <miyazawa-a@nii.ac.jp>
2019-04-06 02:26:57 +0900

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub fn q1() -> u32 {
+    (1..1000).filter(|x| (x % 3 == 0) || (x % 5 == 0)).sum()
+}
+
+pub fn fibonacci(i: u32) -> u32 {
+    if i == 0 || i == 1 {
+        1
+    } else {
+        fibonacci(i - 1) + fibonacci(i - 2)
+    }
+}
+
+pub fn q2() -> u32 {
+    (1u32..)
+        .take_while(|&i| fibonacci(i) <= 4_000_000)
+        .map(|i| fibonacci(i))
+        .filter(|n| n % 2 == 0)
+        .sum()
+}
---
+++ tests/lib.rs
@@ -0,0 +1,21 @@
+extern crate euler;
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_q1() {
+        assert_eq!(euler::q1(), 233168);
+    }
+
+    #[test]
+    fn test_fibonacci() {
+        assert_eq!(euler::fibonacci(6), 13);
+    }
+
+    #[test]
+    fn test_q2() {
+        assert_eq!(euler::q2(), 4613732);
+    }
+
+}
