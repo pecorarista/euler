208668bdcaaa6b84f651b7c8cc36f119102ebd6a
Miyazawa Akira <miyazawa-a@nii.ac.jp>
2019-04-06 21:53:31 +0900

    Add Q4

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,50 @@
-pub fn q1() -> u32 {
+pub fn q1() -> u64 {
     (1..1000).filter(|x| (x % 3 == 0) || (x % 5 == 0)).sum()
 }
 
-pub fn fibonacci(i: u32) -> u32 {
-    if (i == 0) || (i == 1) {
+pub fn fibonacci(i: u64) -> u64 {
+    if i == 0 || i == 1 {
         1
     } else {
         fibonacci(i - 1) + fibonacci(i - 2)
     }
 }
 
-pub fn q2() -> u32 {
-    (1u32..)
-        .take_while(|&i| fibonacci(i) <= 4_000_000)
-        .map(fibonacci)
-        .filter(|n| n % 2 == 0)
-        .sum()
+pub fn q2() -> u64 {
+    let mut v: Vec<u64> = vec![];
+    for i in 1.. {
+        let f = fibonacci(i);
+        v.push(f);
+        if f > 4_000_000 {
+            break;
+        }
+    }
+    v.iter().filter(|&n| n % 2 == 0).sum()
+}
+
+pub fn is_palindromic(n: u64) -> bool {
+    let s = n.to_string();
+    let m = s.len() / 2;
+    let xs = s.bytes().take(m);
+    let ys = s.bytes().rev().take(m);
+    xs.zip(ys).all(|(x, y)| x == y)
+}
+
+pub fn q4() -> u64 {
+    let mut m = 0;
+    for i in 100..999 {
+        for j in (100..999).rev() {
+            if i < j {
+                continue;
+            }
+            let p = i * j;
+            if is_palindromic(p) {
+                if m < p {
+                    m = p;
+                }
+                break;
+            }
+        }
+    }
+    m
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -18,4 +18,34 @@ mod tests {
         assert_eq!(euler::q2(), 4_613_732);
     }
 
+    #[test]
+    fn test_is_palindromic_1() {
+        assert!(euler::is_palindromic(2));
+    }
+
+    #[test]
+    fn test_is_palindromic_3() {
+        assert!(euler::is_palindromic(131));
+    }
+
+    #[test]
+    fn test_is_palindromic_3_not() {
+        assert!(!euler::is_palindromic(123));
+    }
+
+    #[test]
+    fn test_is_palindromic_4() {
+        assert!(euler::is_palindromic(2332));
+    }
+
+    #[test]
+    fn test_is_palindromic_4_not() {
+        assert!(!euler::is_palindromic(2323));
+    }
+
+    #[test]
+    fn test_q4() {
+        assert_eq!(euler::q4(), 906_609);
+    }
+
 }
